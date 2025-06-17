29907b420c8b7cb2fb2a5c8864cc87816aa84960
stunnerboyone <stunnerboy691@gmial.com>
2025-06-17 17:25:36 +0300

    test new

---
+++ catalog/language/uk-ua/uk-ua.php
@@ -0,0 +1,78 @@
+<?php
+// Locale
+$_['code']                  = 'uk';
+$_['direction']             = 'ltr';
+$_['date_format_short']     = 'd.m.Y';
+$_['date_format_long']      = 'l, d F Y';
+$_['time_format']           = 'H:i:s';
+$_['datetime_format']       = 'd/m/Y H:i:s';
+$_['decimal_point']         = '.';
+$_['thousand_point']        = ' ';
+
+// Text
+$_['text_home']             = '<i class="fa-solid fa-house"></i>';
+$_['text_yes']              = 'Так';
+$_['text_no']               = 'Ні';
+$_['text_none']             = ' --- Немає --- ';
+$_['text_select']           = ' --- Оберіть --- ';
+$_['text_all_zones']        = 'Усі регіони';
+$_['text_pagination']       = 'Показано з %d по %d із %d (всього сторінок: %d)';
+$_['text_loading']          = 'Завантаження...';
+$_['text_no_results']       = 'Нічого не знайдено!';
+$_['text_instock']          = 'В наявності';
+$_['text_outstock']         = 'Немає в наявності';
+$_['text_productavail']     = 'Товар є в наявності';
+
+// Buttons
+$_['button_address_add']    = 'Додати адресу';
+$_['button_back']           = 'Назад';
+$_['button_continue']       = 'Продовжити';
+$_['button_cart']           = 'У кошик';
+$_['button_cancel']         = 'Скасувати';
+$_['button_compare']        = 'Додати до порівняння';
+$_['button_wishlist']       = 'До списку бажань';
+$_['button_checkout']       = 'Оформити замовлення';
+$_['button_confirm']        = 'Підтвердити замовлення';
+$_['button_coupon']         = 'Застосувати купон';
+$_['button_delete']         = 'Видалити';
+$_['button_download']       = 'Завантажити';
+$_['button_edit']           = 'Редагувати';
+$_['button_filter']         = 'Фільтр';
+$_['button_new_address']    = 'Нова адреса';
+$_['button_change_address'] = 'Змінити адресу';
+$_['button_reviews']        = 'Відгуки';
+$_['button_write']          = 'Написати відгук';
+$_['button_login']          = 'Увійти';
+$_['button_update']         = 'Оновити';
+$_['button_remove']         = 'Видалити';
+$_['button_reorder']        = 'Повторити замовлення';
+$_['button_return']         = 'Повернення';
+$_['button_shopping']       = 'Продовжити покупки';
+$_['button_search']         = 'Пошук';
+$_['button_shipping']       = 'Розрахувати доставку';
+$_['button_submit']         = 'Надіслати';
+$_['button_guest']          = 'Оформити без реєстрації';
+$_['button_view']           = 'Перегляд';
+$_['button_voucher']        = 'Застосувати подарунковий сертифікат';
+$_['button_upload']         = 'Завантажити файл';
+$_['button_reward']         = 'Застосувати бонусні бали';
+$_['button_quote']          = 'Отримати ціну доставки';
+$_['button_list']           = 'Список';
+$_['button_grid']           = 'Сітка';
+$_['button_map']            = 'Показати на мапі';
+$_['button_quickview']      = 'Швидкий перегляд';
+
+// Error
+$_['error_exception']       = 'Код помилки (%s): %s у файлі %s на рядку %s';
+$_['error_upload_1']        = 'Увага: Розмір файлу перевищує максимально допустимий в php.ini!';
+$_['error_upload_2']        = 'Увага: Розмір файлу перевищує обмеження MAX_FILE_SIZE у формі!';
+$_['error_upload_3']        = 'Увага: Файл було завантажено лише частково!';
+$_['error_upload_4']        = 'Увага: Файл не було завантажено!';
+$_['error_upload_6']        = 'Увага: Відсутня тимчасова папка!';
+$_['error_upload_7']        = 'Увага: Не вдалося записати файл на диск!';
+$_['error_upload_8']        = 'Увага: Завантаження файлу зупинено через розширення!';
+$_['error_upload_999']      = 'Увага: Невідома помилка!';
+$_['error_curl']            = 'CURL: Код помилки (%s): %s';
+
+// Datepicker
+$_['datepicker']            = 'uk-ua';
---
+++ testion.php
@@ -0,0 +1,17 @@
+<?php //00363
+// IONCUBE ENCODER 14.0 EVALUATION
+// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
+// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPzPI+rwlfz+HjkjkVN8HSL1bI5nawUyxtggum254CvFg8F+PtCFG/w0HWYF5yfqmJ7+mxbsd
+c895BOL0QJEB0Pxz+dso3htIIDHzcXqsSpzuESxksXVHpxnVqKc+5WzN72KpBWePb8hYfmPFYMKJ
+UVHVuzRk8HCakMRSOiNbxa8mXW175+qAR9x5zajfM6GpeCawm94N1nzRAV6QbitzRn+bFiTkODMU
+gy0SM1yXJSM12a+0l8oe3hdtJYKq8br/c/vJw8N60yqv/HpqtOEwY+CVMGnfybfx+daAjs7qgm8Q
+WnGmCV37KNLGbMw7jvR6ZfdsxZ9cqFdXaqMWpLDQ8AfH4AMjPnQ1wC5XHH5TVYvZh26Bs16GBHJD
+o2jbAj1QB38NGP6IpGvh8zCBzqtd13tXeiDHjwhmP63egbwHn2GuVhh48pcLNLC1kYk82xM895T2
+Cs4UqlLgw1+hkJCXdzt9D0WH1WAtgTgy0xp9o6ho/md8alEVpD14M9GnGJPbkIPRihGT74rkqHTQ
+8b4uKtwf7HCU2XaG4yHbuxw+S8UYErSrMDGrwGtSgB8h84Jv6Wmc0P3RJEk3X2f9Y5vFIqJqNG3f
+Wrtx/i0CbpqXDGxJpXP8nPn77cbqhcdgmPEEWxHoB+ppdrr5vDt7/Vv2wGBX+hI8BRyRRipUJPQ5
+7obFrm8jGpk9ZqZTocpvFLLCf7Y+mkpqKN1E/zZs6Y6vbJ9TWdSGwk/v7VsgA2ybh/kU5se=
